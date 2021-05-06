f9db2c53661e850974ceff86eab30af3536df763
dbonach <deivitlopes@gmail.com>
2021-05-06 11:10:12 -0300

    Add Auction model

--- auctions/models.py
+++ auctions/models.py
@@ -4,3 +4,13 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+class Auction(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.TextField(blank=True)
+    url = models.URLField(null=True)
+    category = models.CharField(max_length=20, null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="items")   
+
+    def __str__(self):
+        return f"{self.title}" 
